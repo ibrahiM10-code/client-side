36e8f89b843553daf54ea1a5021afdd62060ce94
ibrahiM10 <ibrahim.sma1206@gmail.com>
2024-10-20 13:44:10 -0300

    Modify login page styles.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
     return (
