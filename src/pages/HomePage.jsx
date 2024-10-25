69b0c39dd42048fe091333a5c806f1539733c094
ibrahiM10 <ibrahim.sma1206@gmail.com>
2024-10-24 21:14:42 -0300

    Finish styling the menu.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 function HomePage() {
     return (
@@ -18,29 +19,43 @@ function HomePage() {
                         </div>
                     </div>
                     <div className="payment-methods-container option">
-                        {/* <div className="payment-method-content">
-                            <h1>My Payment Methods</h1>
-                            <img src="./images/money.png" alt="money icon" />
-                            <img src="./images/cards.png" alt="cards icon" />
-                        </div> */}
+                        <div className="payment-method-content">
+                            <div className="option-legend">
+                                <p>My Payment</p>
+                                <p>Methods</p>
+                            </div>
+                            <div className="payment-methods-img-container">
+                                <img src="./images/cards.png" alt="cards icon" />
+                                <img src="./images/money.png" alt="money icon" id="money-img-methods"/>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="second-row-options">    
                     <div className="add-expense-container option">
-                        {/* <div className="add-expense-content">
-                            <h1>Add Expense</h1>
-                            <img src="./images/red-arrow.png" alt="red arrow" />
-                            <img src="./iamges/money-bag.png" alt="money bag icon" />
-                        </div> */}
+                        <div className="add-expense-content">
+                            <div className="option-legend">
+                                <p>Add</p>
+                                <p>Expense</p>
+                            </div>
+                            <div className="add-expense-img-container">
+                                <img src="./images/money-bag.png" alt="money bag icon" />
+                                <img src="./images/red-arrow.png" alt="red arrow" id="red-arrow-add"/>
+                            </div>
+                        </div>
                     </div>
                     <div className="history-container option">
-                        {/* <div className="history-content">
-                            <h1>Expenses History</h1>
+                        <div className="history-content">
+                            <div className="option-legend">
+                                <p>Expenses</p>
+                                <p>History</p>
+                            </div>
                             <img src="./images/bar.png" alt="bars graph icon" />
-                        </div> */}
+                        </div>
                     </div>
                 </div>
             </div>
+            <Footer />
         </>
     )
 }
