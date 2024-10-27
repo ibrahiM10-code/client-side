ed0ec181d278cfd955fc6ed7b1aeef0d92903621
ibrahiM10 <ibrahim.sma1206@gmail.com>
2024-10-26 22:06:15 -0300

    Add password validations and alerts.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, {useState}  from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
 
 function LoginPage() {
+
+    const [isActive, setIsActive] = useState(false);
+
+    const logUser = (e) => {
+        console.log("Logged in!");
+        e.preventDefault();
+    }
+    
     return (
         <>
             <NavBar loggedIn={false}/>
-            <div class="form-container">
+            <div className="form-container">
                 <h1>Iniciar Sesión</h1>
-                <form id="login-form" method="post">
-                    <label for="email">Correo electrónico</label>
+                <form id="login-form" onSubmit={logUser}>
+                    <label htmlFor="email">Correo electrónico</label>
                     <input type="email" id="email" name="email" required />
 
-                    <label for="password">Contraseña</label>
+                    <label htmlFor="password">Contraseña</label>
                     <input type="password" id="password" name="password" required />
 
                     <button type="submit">Iniciar Sesión</button>
                 </form>
                 <p>¿No tienes una cuenta? <Link to="/register">Registrarse</Link></p>
             </div>
+            {isActive && (
+                <Stack className="alert-register" sx={{ width: '20%' }} spacing={2}>
+                    <Alert severity="info" onClose={() => {}} onClick={ () => {setIsActive(false)}}>
+                        The password must be of 8 characters long.
+                    </Alert>
+                </Stack>
+             )}
             <Footer />
         </>
     )
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,32 +1,65 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
 
 function RegisterPage() {
+
+    const [password, setPassword] = useState("");
+    const [isActive, setIsActive] = useState(false);
+
+    const handlePassword = (event) => {
+        const pass = event.target.value;
+        setPassword(pass)
+    }
+
+    const validatePassword = (password) => {
+        if (password.length <= 7) {
+            return "Password is too short."
+        }
+    }
+
+    const registerUser = (event) => {
+        const error = validatePassword(password);
+        if (error) {
+            setIsActive(true)
+        }
+        event.preventDefault();
+    }
+
+
     return (
         <>
             <NavBar loggedIn={false} />
-            <div class="form-container">
+            <div className="form-container">
                 <h1>Registro</h1>
-                <form id="registration-form" method="post">
-                    <label for="firstName">Nombre</label>
+                <form id="registration-form" method="post" onSubmit={registerUser}>
+                    <label htmlFor="firstName">Nombre</label>
                     <input type="text" id="firstName" name="firstName" required />
 
-                    <label for="lastName">Apellido</label>
+                    <label htmlFor="lastName">Apellido</label>
                     <input type="text" id="lastName" name="lastName" required />
 
-                    <label for="email">Correo electrónico</label>
+                    <label htmlFor="email">Correo electrónico</label>
                     <input type="email" id="email" name="email" required />
 
-                    <label for="password">Contraseña</label>
-                    <input type="password" id="password" name="password" required />
+                    <label htmlFor="password">Contraseña</label>
+                    <input type="password" id="password" name="password" onChange={handlePassword} required />
 
                     <button type="submit">Registrar</button>
                 </form>
                 <p>¿Ya tienes una cuenta? <Link to="/login">Inicia Sesión</Link></p>
              </div>
-             <Footer />
+             {isActive && (
+                <Stack className="alert-register" sx={{ width: '20%' }} spacing={2}>
+                    <Alert severity="info" onClose={() => {}} onClick={ () => {setIsActive(false)}}>
+                        The password must be of 8 characters long.
+                    </Alert>
+                </Stack>
+             )}
+            <Footer />
         </>
     )
 }
