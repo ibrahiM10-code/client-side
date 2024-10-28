7b70f7db6b101bcc53d04f4d9f3a902667d9b71b
ibrahiM10 <ibrahim.sma1206@gmail.com>
2024-10-27 21:01:14 -0300

    Landing Page setup.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import RegisterPage from "../pages/RegisterPage";
 import LoginPage from "../pages/LoginPage";
 import HomePage from "../pages/HomePage";
+import LandingPage from "../pages/LandingPage";
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
         <Route path="/register" element={<RegisterPage />}></Route>
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/home" element={<HomePage/>}></Route>
