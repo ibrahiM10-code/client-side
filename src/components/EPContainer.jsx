a1a18fbd8da7b9b798b7d6c3f3ede5a437a4f3bc
ibrahiM10 <ibrahim.sma1206@gmail.com>
2024-11-02 15:45:21 -0300

    Add form modals for the expenses and payment methods.

--- src/components/EPContainer.jsx
+++ src/components/EPContainer.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import FormModal from "./FormModal";
 
 function EPContainer({ children, title, btnText }) {
+  const [open, setOpen] = useState(false);
+  const openTheModal = () => {
+    setOpen(true);
+  };
+
   return (
     <>
       <div className="pms-container">
         <div className="pms-header">
           <h3>{title}</h3>
-          <button className="modal-form-btn">{btnText}</button>
+          <button className="modal-form-btn" onClick={openTheModal}>
+            {btnText}
+          </button>
         </div>
         <hr />
         <div className="user-pmtds-container">{children}</div>
       </div>
+      {title === "your expenses" ? (
+        <FormModal
+          formTitle={"Add a new expense."}
+          openFormModal={open}
+          setOpenFormModal={setOpen}
+        />
+      ) : (
+        <FormModal
+          formTitle={"Add a new payment method."}
+          openFormModal={open}
+          setOpenFormModal={setOpen}
+        />
+      )}
     </>
   );
 }
