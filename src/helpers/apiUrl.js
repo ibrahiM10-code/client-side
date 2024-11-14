fbbcc6bf90447ee703754a0fb1118d09accbdec4
ibrahiM10 <ibrahim.sma1206@gmail.com>
2024-11-14 20:12:59 -0300

    API URL restored.

--- src/helpers/apiUrl.js
+++ src/helpers/apiUrl.js
@@ -1,2 +1,2 @@
-export const apiAuthUrl = "https://23.22.14.225:3001/api/auth";
-export const apiProcessUrl = "https://23.22.14.225:3001/api";
+export const apiAuthUrl = "http://localhost:3001/api/auth";
+export const apiProcessUrl = "http://localhost:3001/api";
