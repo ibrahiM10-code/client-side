26f9d7ad985813bdce69ee6363c361276dec8a82
ibrahiM10 <ibrahim.sma1206@gmail.com>
2024-11-14 19:15:55 -0300

    Changes in the API URL.

--- src/helpers/apiUrl.js
+++ src/helpers/apiUrl.js
@@ -1,2 +1,2 @@
-export const apiAuthUrl = "http://localhost:3001/api/auth";
-export const apiProcessUrl = "http://localhost:3001/api";
+export const apiAuthUrl = "http://23.22.14.225:3001/api/auth";
+export const apiProcessUrl = "http://23.22.14.225:3001/api";
