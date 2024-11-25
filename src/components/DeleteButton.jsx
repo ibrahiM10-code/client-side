375c2e55bec1f5943c4194c4fef4f15bc8b6937f
ibrahiM10 <ibrahim.sma1206@gmail.com>
2024-11-25 14:55:15 -0300

    Delete button update.

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
+import AuthContext from "../context/AuthProvider";
+import { apiProcessUrl } from "../helpers/apiUrl";
+import axios from "axios";
+import DeleteModal from "./DeleteModal";
 
-function DeleteButton() {
+function DeleteButton({ payment_method_id }) {
+  const { config } = useContext(AuthContext);
   const openModal = () => {
     document.getElementById("modal").classList.remove("d-none");
   };
 
+  const deletePaymentMethod = async () => {
+    try {
+      console.log(payment_method_id);
+      const response = await axios.delete(
+        `${apiProcessUrl}/payment-method/${payment_method_id}`,
+        config
+      );
+      if (response.status === 204) {
+        alert("Payment method deleted!");
+        console.log(response);
+      }
+    } catch (error) {
+      alert("Failed to delete the payment method");
+      console.error(error);
+    }
+  };
+
   return (
-    <button className="delete-button text-danger" onClick={openModal}>
-      <i className="fas fa-trash-alt"></i>
-    </button>
+    <>
+      <button
+        className="delete-button text-danger"
+        onClick={deletePaymentMethod}
+      >
+        <i className="fas fa-trash-alt"></i>
+      </button>
+      {/* <DeleteModal /> */}
+    </>
   );
 }
 
