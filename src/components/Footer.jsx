33622e6a2e19a4571a83da1becc32fd4fa0f5c25
ibrahiM10 <ibrahim.sma1206@gmail.com>
2024-12-12 12:52:45 -0300

    Footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
 function Footer() {
-    return (
-        <footer>
-            <p>© 2024 BC Finances</p>
-            <hr />
-            <p>Created By: Arthut Marcon, Fabian Galaz, Giovanni Cacioli, Ibrahim Muhammad and Jordan Rojo.</p>
-        </footer>
-    )
+  return (
+    <footer>
+      <p>© 2024 BC Finances</p>
+      <hr />
+      <p>
+        Created By: Arthur Marcon, Fabian Galaz, Giovanni Cacioli, Ibrahim
+        Muhammad and Jordan Rojo.
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
